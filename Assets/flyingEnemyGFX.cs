b3ba2f9e7a5c44d51bb77e7ea11a12086c6ffa6f
simondockal <101214260+simondockal@users.noreply.github.com>
2023-03-08 21:20:26 +0100

    FlyingEnemySCRPT

---
+++ Assets/flyingEnemyGFX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class flyingEnemyGFX : MonoBehaviour
+{
+    public AIPath aipath;
+    void Update()
+    {
+        if (aipath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (aipath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+}
